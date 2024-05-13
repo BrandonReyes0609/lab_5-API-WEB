2cc6825e5980a07acb29c1e13293148f96f99962
BrandonReyes0609 <101024720+BrandonReyes0609@users.noreply.github.com>
2024-05-12 21:34:48 -0600

    feat(schema): add new tables to database schema
    Este mensaje indica que se han añadido nuevas tablas al esquema de la base de datos, lo que podría ser necesario para nuevas características que requieran almacenamiento de datos adicionales.

--- schema.sql
+++ schema.sql
@@ -1,8 +1,11 @@
-CREATE TABLE IF NOT EXISTS blog_posts ( 
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-    banner TEXT not NULL,
-    locationU TEXT not NULL,
-    cost TEXT not NULL
+-- Configuracion de la base de datos
+
+CREATE TABLE IF NOT EXISTS post (
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+	title VARCHAR(255) NOT NULL,
+	content TEXT NOT NULL,
+	banner TEXT not NULL,
+	locationU TEXT not NULL
 );
+
+
