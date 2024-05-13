012a0d78491bb8b319e65ceb6e9fec7c4964b4d2
BrandonReyes0609 <101024720+BrandonReyes0609@users.noreply.github.com>
2024-05-12 22:03:41 -0600

    refactor(src/conn): improve database connection efficiency
    Este mensaje indica una mejora en la eficiencia de la conexión a la base de datos, lo que podría implicar la optimización de parámetros de conexión o el manejo de conexiones.

--- src/conn.js
+++ src/conn.js
@@ -1,79 +1,15 @@
-// conn.js
-
-import mysql from 'mysql2/promise';
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'blog_comida_uvg',
-    password: '116611',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-
-// Función para probar la conexión al iniciar la aplicación
-async function testConnection() {
-    try {
-        const connection = await pool.getConnection();
-        console.log('Conexión exitosa a la base de datos!');
-        connection.release();  // Es importante liberar la conexión
-    } catch (error) {
-        console.error('Error al conectar a la base de datos:', error);
-    }
-}
-
-testConnection();
-
-export default pool;
-/*
-//---------------------------- ultima coneccion
-import mysql from 'mysql2/promise';
+//			Conección con DB
+const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'mysql',
-    database: 'blog_comida_uvg',
-    password: '116611',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-
-export default pool;
-//________________________
-*/
-/*
-//-----------------------------
-import mysql from 'mysql2/promise'
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'mysql',
-    database: 'blog_comida_uvg',
-    password: '116611',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: 'localhost',
+  port: 3306,
+  user: 'brandon',
+  database: 'post_db',
+  password: '123',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 })
 
-export default pool
-//----------------------------
-
-*/
-/*
-import mysql from 'mysql2/promise'; // Asegúrate de incluir el punto y coma al final
-
-// La configuración de la conexión debe estar correctamente indentada
-const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'mariadb',
-  database: 'blog_comida_uvg',
-  password: '116611',
-  waitForConnections: true,
-  connectionLimit: 10, // La opción debe ser en minúsculas
-  queueLimit: 0,
-}); // Añadir una coma al final y punto y coma
-
-export default pool; // Punto y coma al final
-*/
+module.exports = pool
